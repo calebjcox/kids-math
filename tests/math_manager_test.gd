a938d19ee70bb77fc4147f30597d5fb154d5f880
Caleb <caleb@calebcox.net>
2020-06-20 14:55:56 -0500

    Expanded test scripts to cover subtracton and counting

--- tests/math_manager_test.gd
+++ tests/math_manager_test.gd
@@ -2,8 +2,13 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_test_exercise_type(MathManager.MathMode.ADDITION)
+	_test_exercise_type(MathManager.MathMode.SUBTRACTION)
+	_test_exercise_type(MathManager.MathMode.COUNTING)
+
+func _test_exercise_type(type = MathManager.MathMode.ADDITION):
 	var exercise : _Operator
-	var manager = MathManager.new()
+	var manager = MathManager.new(type)
 	while !manager.finished():
 		exercise = manager.nextExercise()
 		print(exercise)
