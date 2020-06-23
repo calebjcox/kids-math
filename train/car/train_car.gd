90adc3062588fe6e5acb59d577e2d150bf7870b5
Caleb <caleb@calebcox.net>
2020-06-22 21:13:07 -0500

    INCOMPLETE: initial train car scene code

---
+++ train/car/train_car.gd
@@ -0,0 +1,31 @@
+class_name TrainCar
+extends Sprite
+
+enum CarColor {
+	AQUA,
+	BLUE,
+	FUSHIA,
+	GREEN,
+	LIME,
+	MAROON,
+	NAVY,
+	PURPLE,
+	RED,
+	TEAL,
+	YELLOW,
+}
+enum CarDirection {
+	LEFT,
+	RIGHT,
+}
+
+export var color : CarColor
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
