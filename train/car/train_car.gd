05c005d0865f8977bcab13a897de8ee40ba4957f
Caleb <caleb@calebcox.net>
2020-06-22 22:36:11 -0500

    Basic Train Car Scene

--- train/car/train_car.gd
+++ train/car/train_car.gd
@@ -1,31 +1,83 @@
 class_name TrainCar
 extends Sprite
 
-enum CarColor {
-	AQUA,
-	BLUE,
-	FUSHIA,
-	GREEN,
-	LIME,
-	MAROON,
-	NAVY,
-	PURPLE,
-	RED,
-	TEAL,
-	YELLOW,
-}
-enum CarDirection {
-	LEFT,
-	RIGHT,
-}
-
-export var color : CarColor
+export(String, 
+	"aqua", 
+	"blue", 
+	"fushia", 
+	"green", 
+	"lime", 
+	"maroon", 
+	"navy", 
+	"purple", 
+	"red", 
+	"teal", 
+	"yellow"
+) var color = "aqua" setget _setColor
+export(String, "left", "right") var direction = "left" setget _setDirection
 
 
 func _ready():
-	pass # Replace with function body.
+	loadTexture()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func changeCar(newColor := "aqua", newDirection := "left") -> void:
+	if !_validateDirection(newDirection):
+		return
+	if !_validateColor(newColor):
+		return
+	direction = newDirection
+	color = newColor
+	loadTexture()
+
+
+func loadTexture() -> void:
+	self.texture = _trainCarTexture()
+
+
+func _trainCarTexture() -> Resource:
+	return load(_trainCarResPath());
+
+
+func _trainCarResPath() -> String:
+	return "res://train/car/" + direction + "/train_car_" + direction + "_" + color + ".png"
+
+
+func _setColor(newColor : String) -> void:
+	if !_validateColor(newColor):
+		return
+	color = newColor
+	loadTexture()
+
+
+func _setDirection(newDirection : String) -> void:
+	if !_validateDirection(newDirection):
+		return
+	direction = newDirection
+	loadTexture()
+
+
+func _validateColor(checkColor) -> bool:
+	match checkColor:
+		"aqua", \
+		"blue", \
+		"fushia", \
+		"green", \
+		"lime", \
+		"maroon", \
+		"navy", \
+		"purple", \
+		"red", \
+		"teal", \
+		"yellow":
+			return true
+		_:
+			return false
+
+func _validateDirection(checkDirection) -> bool:
+	match checkDirection:
+		"left", \
+		"right":
+			return true
+		_:
+			return false
