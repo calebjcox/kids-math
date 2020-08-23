e1d897bf92ead89e63a81e3916fe05b8c9682cf3
Caleb <caleb@calebcox.net>
2020-08-23 15:38:35 -0500

    Allow for dynamically adjusting the number of cars in each group

--- train/car/train_car_group.gd
+++ train/car/train_car_group.gd
@@ -2,7 +2,7 @@ class_name TrainCarGroup
 extends Container
 
 onready var car = preload("train_car.tscn")
-export(int) var number_of_cars = 1
+export(int) var number_of_cars = 1 setget _setCars
 
 export(String,
 	"left",
@@ -10,13 +10,16 @@ export(String,
 ) var direction = "right" setget _setDirection
 
 func _ready():
-	var newCar: TrainCar
-	
 	for _i in range(number_of_cars):
-		newCar = car.instance()
-		newCar.direction = direction
-		newCar.randomColor()
-		$HBoxContainer.add_child(newCar)
+		$HBoxContainer.add_child(_newCar())
+
+
+func _newCar() -> TrainCar:
+	var newCar: TrainCar
+	newCar = car.instance()
+	newCar.direction = direction
+	newCar.randomColor()
+	return newCar
 
 
 func _setDirection(newDirection):
@@ -24,3 +27,12 @@ func _setDirection(newDirection):
 	if is_inside_tree():
 		for car in $HBoxContainer.get_children():
 			car.direction = direction
+
+
+func _setCars(number):
+	if number < 0:
+		return
+	while $HBoxContainer.get_child_count() > number:
+		$HBoxContainer.remove_child($HBoxContainer.get_child($HBoxContainer.get_child_count() - 1))
+	while $HBoxContainer.get_child_count() < number:
+		$HBoxContainer.add_child(_newCar())
--- train/train.gd
+++ train/train.gd
@@ -2,6 +2,11 @@ class_name Train
 extends Node
 
 
+export (int, 1, 2) var number_of_car_sections = 2
+export (int) var number_of_cars_section1 = 1
+export (int) var number_of_cars_section2 = 1
+
+
 export(String,
 	"left",
 	"right"
@@ -10,6 +15,18 @@ export(String,
 
 func _ready():
 	_updateDirection()
+	_updateCarGroups()
+
+
+func setNumberOfCars(first: int, second: int = 0):
+	number_of_cars_section1 = first
+	if second > 0:
+		number_of_cars_section2 = second
+		number_of_car_sections = 2
+	else:
+		number_of_car_sections = 1
+	if is_inside_tree():
+		_updateCarGroups()
 
 
 func _setDirection(newDirection):
@@ -26,3 +43,15 @@ func _updateDirection():
 	$TrainEngine.direction = direction
 	$CarGroup1.direction = direction
 	$CarGroup2.direction = direction
+
+
+func _updateCarGroups():
+	$CarGroup1.number_of_cars = number_of_cars_section1
+	
+	if number_of_car_sections == 1:
+		$CarGroup2.visible = false
+		$CarSpacer.visible = false
+	else:
+		$CarGroup2.visible = true
+		$CarSpacer.visible = true
+		$CarGroup2.number_of_cars = number_of_cars_section2
