20c52697673fa2887d2fe7025677eab37c6b75a0
Caleb Cox <44181972+calebjcox@users.noreply.github.com>
2020-09-13 16:28:25 -0500

    initial settings class. This is still giving an error when attempting to reference the Settings class.   - The identifier "Settings" isn't declared in the current scope.

---
+++ globals/settings.gd
@@ -0,0 +1,11 @@
+# Singleton to hold all of the settings
+
+var repeat_missed_exercises := true setget set_repeat_missed_exercises, get_repeat_missed_exercises
+
+
+func set_repeat_missed_exercises(repeat: bool):
+	repeat_missed_exercises = repeat
+
+
+func get_repeat_missed_exercises() -> bool:
+	return repeat_missed_exercises
--- home/home.gd
+++ home/home.gd
@@ -1,5 +1,9 @@
 extends Node
 
 
+func _ready():
+	$BoundingContainer/MarginContainer/VBoxContainer/Options/Repeat.pressed = Settings.get_repeat_missed_exercises()
+
+
 func _on_Start_pressed():
 	get_tree().change_scene_to(preload("res://math/math.tscn"))
